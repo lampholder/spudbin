2d1a2568937077bdc89e88e29487e4549271a803
Tom Lant <lampholder@gmail.com>
2017-04-26 17:51:59 +0100

    send the params in the right order for maximum workings

--- spudbin/storage/Associations.py
+++ spudbin/storage/Associations.py
@@ -39,7 +39,7 @@ class Associations(Store):
         print 'asked to update to ', association
         cursor = self._connection.cursor()
         sql = 'update human_templates set start_date = ?, end_date = ? where pkey = ?'
-        cursor.execute(sql, (association.pkey, association.start_date, association.end_date, ))
+        cursor.execute(sql, (association.start_date, association.end_date, association.pkey, ))
         self._connection.commit()
         cursor.close()
 
