4d4838bacb247251e0178f11f47e5c450d707c24
Tom Lant <lampholder@gmail.com>
2017-05-05 19:22:51 +0100

    302 loop fail

--- spudbin/app/ui/Ui.py
+++ spudbin/app/ui/Ui.py
@@ -13,7 +13,7 @@ def authenticated(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
         """PaRapper the wrapper"""
-        if 'github_token' not in request.cookies or 'github_login' not in request.cookies:
+        if 'github_auth_token' not in request.cookies or 'github_login' not in request.cookies:
             session['target_url'] = request.url
             print 'persisting destination', session['target_url']
             return redirect_to_github()
