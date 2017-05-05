728868fdb4facd6293927d588282b5f6976d99a3
Tom Lant <lampholder@gmail.com>
2017-05-05 11:47:07 +0100

    Trying to do auth right

--- spudbin/app/__init__.py
+++ spudbin/app/__init__.py
@@ -7,4 +7,4 @@ from spudbin.util import DateConverter
 app = Flask(__name__)
 
 app.url_map.converters['date'] = DateConverter
-app.secret_key = 'He\'s a lumberjack and he\'s okay'
+app.secret_key = 'n\xe1\x87\xaaX\xff\xcb\x07\x10\xec\xac\xccX;\x0e\x1f,\xcd\xb2\x8drKf\xa9'
