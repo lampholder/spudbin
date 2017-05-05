a38d2098fe275cf70cac8ed9e6fdea583deb209d
Tom Lant <lampholder@gmail.com>
2017-05-05 12:35:14 +0100

    Trying to do auth right

--- spudbin/app/ui/Ui.py
+++ spudbin/app/ui/Ui.py
@@ -1,16 +1,23 @@
-#TODO: coding
+"""All the UI gubbins"""
+from spudbin.app import app
 
-def is_authed(username):
-    if 'github_token' not in session:
-        return False
-    with Database.connection() as connection:
-        users = USERS.fetch_by_username(username, connection)
-    if users is None:
-        return False #XXX: Gotta do better than this
-    auth_test = requests.get('https://api.github.com/user',
-                             params={'access_token': session['github_token']})
-    return auth_test.status_code != 200
+@app.route('/potatoes', methods=['GET'])
+def submit_tokens():
+    """UI for submitting tokens"""
+    return 'Hello, world', 200
 
-def bauthenticate(username):
-    if not is_authed(username):
-        return redirect('https://spudb.in/login', code='302')
+
+#def is_authed(username):
+#    if 'github_token' not in session:
+#        return False
+#    with Database.connection() as connection:
+#        users = USERS.fetch_by_username(username, connection)
+#    if users is None:
+#        return False #XXX: Gotta do better than this
+#    auth_test = requests.get('https://api.github.com/user',
+#                             params={'access_token': session['github_token']})
+#    return auth_test.status_code != 200
+#
+#def bauthenticate(username):
+#    if not is_authed(username):
+#        return redirect('https://spudb.in/login', code='302')
