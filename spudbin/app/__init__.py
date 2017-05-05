4b1c1a75182e6277cee62243f3180c3c91dbdc86
Tom Lant <lampholder@gmail.com>
2017-05-05 14:25:42 +0100

    UI time

--- spudbin/app/__init__.py
+++ spudbin/app/__init__.py
@@ -7,4 +7,12 @@ from spudbin.util import DateConverter
 app = Flask(__name__)
 
 app.url_map.converters['date'] = DateConverter
+
 app.secret_key = 'n\xe1\x87\xaaX\xff\xcb\x07\x10\xec\xac\xccX;\x0e\x1f,\xcd\xb2\x8drKf\xa9'
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
