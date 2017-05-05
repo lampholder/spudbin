0dd68f3cdc400520fb4530b30f6d9e5d03a4d147
Tom Lant <lampholder@gmail.com>
2017-05-05 16:36:42 +0100

    Make the ui bounce people via login

--- spudbin/app/ui/Ui.py
+++ spudbin/app/ui/Ui.py
@@ -1,25 +1,25 @@
 """All the UI gubbins"""
+from functools import wraps
+
+from flask import session
 from flask import render_template
 
 from spudbin.app import app
+from spudbin.app.auth.GithubLogin import redirect_to_github
 
-@app.route('/potatoes', methods=['GET'])
+def authenticated(func):
+    """Checks that we have a github token - if not, bounce via login to get one"""
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        """PaRapper the wrapper"""
+        if 'github_token' not in session:
+            return redirect_to_github()
+
+        return func(*args, **kwargs)
+    return wrapped
+
+@app.route('/record', methods=['GET'])
+@authenticated
 def ui_submit_tokens():
     """UI for submitting tokens"""
-    return render_template('potatoes.html', username='xyzzy')
-
-
-#def is_authed(username):
-#    if 'github_token' not in session:
-#        return False
-#    with Database.connection() as connection:
-#        users = USERS.fetch_by_username(username, connection)
-#    if users is None:
-#        return False #XXX: Gotta do better than this
-#    auth_test = requests.get('https://api.github.com/user',
-#                             params={'access_token': session['github_token']})
-#    return auth_test.status_code != 200
-#
-#def bauthenticate(username):
-#    if not is_authed(username):
-#        return redirect('https://spudb.in/login', code='302')
+    return render_template('record.html', username='xyzzy')
